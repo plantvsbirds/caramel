26be17c98bda405c9026f5bda3a28767529ad276
Kedan Li <likedan5@icloud.com>
2017-09-12 23:19:00 -0700

    add 6 samples

--- ConvertModel/CreateSampleTemplate.py
+++ ConvertModel/CreateSampleTemplate.py
@@ -21,4 +21,4 @@ for file_name in os.listdir(metadata_files):
 
         with open(os.path.join(output_dir, file_name), 'w') as outfile:
             json.dump(result, outfile, indent=4)
-        os.mkdir(os.path.join(output_dir, file_name.strip(".json")))
+        os.mkdir(os.path.join(output_dir, file_name.replace(".json", "")))
