b827db69d19ccb50a03403dd9182c86ed6c7d001
plantvsbirds <plantvsbird@gmail.com>
2017-09-13 10:19:15 -0700

    Extract type logic to util

--- src/components/MLFeatureType.jsx
+++ src/components/MLFeatureType.jsx
@@ -3,7 +3,7 @@ import classNameBind from 'classnames/bind'
 import styles from './MLFeatureType.styl'
 
 const cx = classNameBind.bind(styles)
-import { types } from '../const'
+import { getTypeShorthand } from '../utils'
 
 class MLFeature extends Component {
   constructor() {
@@ -11,15 +11,14 @@ class MLFeature extends Component {
   }
   render() {
     const { className, size, type } = this.props
-    const inProps = (k) => {
-      return this.props.type && this.props.type.toLowerCase().indexOf(k) == 0
-    }
     return (
-      <div className={cx(
-        'type',
-        size,
-        className,
-        types.find(k => this.props[k] || inProps(k)))}>
+      <div
+        className={cx(
+          'type',
+          size,
+          className,
+          getTypeShorthand(type)
+        )}>
       </div>
     )
   }
---
+++ src/utils.js
@@ -0,0 +1,6 @@
+import { types } from './const'
+
+export const mapObject = (obj, func) => Object.keys(obj).map(k => func(k, obj[k]))
+
+export const getTypeShorthand = (fullType) =>
+  fullType && types.find(candidate => fullType.toLowerCase().indexOf(candidate) === 0)
