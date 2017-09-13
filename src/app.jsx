f40859959da455c8710b2822529ec03ac3538352
plantvsbirds <plantvsbird@gmail.com>
2017-09-13 10:23:20 -0700

    Plug-in data and add sample mock

--- src/app.jsx
+++ src/app.jsx
@@ -4,6 +4,24 @@ import ReactDOM from 'react-dom'
 import ModelDetail from './pages/model'
 
 // !!mock
-import mockData from '../ConvertModel/MobileNet.json'
+import mockData from '../data/metadata/MobileNet.json'
+mockData.samples = [{
+  "input": {
+      "data": {
+          "type": "image",
+          "url": "https://source.unsplash.com/random/400x300"
+      }
+  }, 
+  "output": {
+    "prob": {
+        "type": "text",
+        "content": "This is the content"
+    },
+    "classLabel": {
+        "type": "text",
+        "content": "This is the content"
+    }
+  }
+}]
 
 export default <ModelDetail model={mockData} />
