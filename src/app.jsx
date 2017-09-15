e6bc126ea650a71e6f66b0c28d6d430b01562717
plantvsbirds <plantvsbird@gmail.com>
2017-09-15 15:09:12 -0700

    Add List page !WIP Render List and Detail on same page

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 
+import ModelList from './pages/list'
 import ModelDetail from './pages/model'
 
+import styles from './pages/page.styl'
 // mock!
 import mockMeta from '../content/metadata/FNS-Mosaic.json'
 import mockSamples from '../content/samples/FNS-Mosaic.json'
 
 export default (
-  <ModelDetail
-    meta={mockMeta}
-    samples={mockSamples}
-  />
+  <div className={styles.container}>
+    <ModelList />
+    <ModelDetail
+      meta={mockMeta}
+      samples={mockSamples}
+    />
+  </div>
 )
