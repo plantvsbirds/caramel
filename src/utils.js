d9a1fde17414fb0d887e384c5aaec6daa8d76c72
plantvsbirds <plantvsbird@gmail.com>
2017-09-17 14:47:11 -0700

    Fix type lookup

--- src/utils.js
+++ src/utils.js
@@ -3,4 +3,4 @@ import { types } from './const'
 export const mapObject = (obj, func) => Object.keys(obj).map(k => func(k, obj[k]))
 
 export const getTypeShorthand = (fullType) =>
-  fullType && types.find(candidate => fullType.toLowerCase().indexOf(candidate) === 0)
+  fullType && types.find(candidate => fullType.toLowerCase().indexOf(candidate.toLowerCase()) === 0)
