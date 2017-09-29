6092b31dcbd183987f7cf60a042ea5c3217a2f1c
Kedan Li <likedan5@icloud.com>
2017-09-29 15:18:12 -0700

    fix samples/ add content generation flow

---
+++ addModel.sh
@@ -0,0 +1,13 @@
+# This gets run after the model metadata is added to the content.json
+
+cd ConvertModel
+python ReadModelInfo.py
+python CreateSampleTemplate.py
+
+# Afterward
+# 1. fill out the sample json file
+# 2. use script to crop thumbnail
+# 3. upload model and image file to s3
+# 4. npm run db
+# 5. generate_readme.py
+# 6. update Awesome Page
